6a7c04bb6b972363f9bbe5b3e72bf39ad30a9a0c
Gaurav Appa <gauravappagga@gmail.com>
2022-02-20 16:46:34 +0530

    Calling First Actor Code

--- src/main/java/com/akkapractice/firstactor/FirstMessageMain.java
+++ src/main/java/com/akkapractice/firstactor/FirstMessageMain.java
@@ -1,5 +1,14 @@
 package com.akkapractice.firstactor;
 
+import akka.actor.typed.ActorSystem;
+
 public class FirstMessageMain {
 
+	public static void main(String[] args) {
+		
+		ActorSystem<String> actorSystem = 
+				ActorSystem.create(FirstAbstractBehavior.create(), "FirstActor");
+		actorSystem.tell("I am first Actor");
+	}
+	
 }
